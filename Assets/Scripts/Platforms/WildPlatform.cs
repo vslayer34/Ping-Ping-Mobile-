c1f81cf87bc0876884c69cd97cc780d218bef091
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-12-29 17:41:59 +0200

    Added the ability to activate the platform sections when spawning the platform

--- Assets/Scripts/Platforms/WildPlatform.cs
+++ Assets/Scripts/Platforms/WildPlatform.cs
@@ -31,9 +31,28 @@ public class WildPlatform : Platform
         //Move(JoystickDirectionY);
     }
 
+    private void OnEnable()
+    {
+        ActivatePlatformSections();
+    }
+
 
     private void OnDisable()
     {
         platformAction?.Disable();
+    }
+
+
+    /// <summary>
+    /// Activate all the indiviual section of the platform
+    /// when enabled after spawning
+    /// </summary>
+    private void ActivatePlatformSections()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+          
     }
 }
